fbd102d8d8758c1b42905a46f305efcb6991ea97
Penar Musaraj <pmusaraj@gmail.com>
2025-02-11 12:18:16 -0500

    first commit

---
+++ com.discourse/README.md
@@ -0,0 +1,6 @@
+Discourse is a community forum platform. Use this connector to see the latest topics (default), or to follow a category or tag.
+
+For categories, use `/c/CATEGORY_SLUG` in the filter input.
+For tags, use `tags/TAG_SLUG` in the filter input.
+
+Learn more at [discourse.org](https://discourse.org)
---
+++ com.discourse/plugin.js
@@ -0,0 +1,492 @@
+// com.discourse
+
+// Adapted from xml.feed with some adjustments
+
+// TODO: Ideally we'd provide UI for category / tag choices
+function buildUrl() {
+  return `${site}/${filter}.rss`;
+}
+
+function verify() {
+  const reqUrl = buildUrl();
+
+  sendRequest(reqUrl)
+    .then((xml) => {
+      console.log("hit response verify?");
+      let jsonObject = xmlParse(xml);
+
+      if (jsonObject.feed != null) {
+        // Atom 1.0
+        const feedAttributes = jsonObject.feed.link$attrs;
+        let baseUrl = null;
+        if (feedAttributes instanceof Array) {
+          for (const feedAttribute of feedAttributes) {
+            if (feedAttribute.rel == "alternate") {
+              baseUrl = feedAttribute.href;
+              break;
+            }
+          }
+        } else {
+          if (feedAttributes.rel == "alternate") {
+            baseUrl = feedAttributes.href;
+          }
+        }
+        const displayName = jsonObject.feed.title;
+        let icon = null;
+        if (jsonObject.feed.icon != null) {
+          icon = jsonObject.feed.icon;
+          const verification = {
+            displayName: displayName,
+            icon: icon,
+            baseUrl: baseUrl,
+          };
+          processVerification(verification);
+        }
+        if (baseUrl != null && icon === null) {
+          let siteUrl = baseUrl.split("/").splice(0, 3).join("/");
+          lookupIcon(siteUrl).then((icon) => {
+            const verification = {
+              displayName: displayName,
+              icon: icon,
+              baseUrl: baseUrl,
+            };
+            processVerification(verification);
+          });
+        } else {
+          // try to get icon from the feed
+          let feedUrl = null;
+          if (feedAttributes instanceof Array) {
+            for (const feedAttribute of feedAttributes) {
+              if (feedAttribute.rel == "self") {
+                feedUrl = feedAttribute.href;
+                break;
+              }
+            }
+          } else {
+            if (feedAttributes.rel == "self") {
+              feedUrl = feedAttributes.href;
+            }
+          }
+          if (feedUrl != null) {
+            let siteUrl = feedUrl.split("/").splice(0, 3).join("/");
+            lookupIcon(siteUrl).then((icon) => {
+              const verification = {
+                displayName: displayName,
+                icon: icon,
+                baseUrl: baseUrl,
+              };
+              processVerification(verification);
+            });
+          } else {
+            const verification = {
+              displayName: displayName,
+              icon: null,
+              baseUrl: baseUrl,
+            };
+            processVerification(verification);
+          }
+        }
+      } else if (jsonObject.rss != null && jsonObject.rss.channel != null) {
+        // RSS 2.0
+        // TODO: Check that XML is good:
+        // if (jsonObject.rss instanceof Object	&& jsonObject.rss.channel instanceof Object) { ... }
+
+        const baseUrl = jsonObject.rss.channel.link;
+        const displayName = jsonObject.rss.channel.title;
+
+        // NOTE: In theory, the channel image could be used to get an icon for the feed. But some
+        // use non-square images that look bad when squished. For example, the New York Times feed
+        // uses a 240x40 image.
+        //			if (jsonObject.rss.channel.image != null) {
+        //				icon = jsonObject.rss.channel.image.url;
+        //				const verification = {
+        //					displayName: displayName,
+        //					icon: icon,
+        //					baseUrl: baseUrl
+        //				};
+        //				processVerification(verification);
+        //			}
+        let feedUrl = baseUrl.split("/").splice(0, 3).join("/");
+        lookupIcon(feedUrl).then((icon) => {
+          const verification = {
+            displayName: displayName,
+            icon: icon,
+            baseUrl: baseUrl,
+          };
+          processVerification(verification);
+        });
+      } else if (jsonObject["rdf:RDF"] != null) {
+        // RSS 1.0
+        const baseUrl = jsonObject["rdf:RDF"].channel.link;
+        const displayName = jsonObject["rdf:RDF"].channel.title;
+
+        // NOTE: In theory, you can get the icon from the RDF channel. In practice, places like
+        // Slashdot haven't updated this image since the beginning of this century.
+        // 			if (jsonObject["rdf:RDF"].channel.image$attrs != null) {
+        // 				icon = jsonObject["rdf:RDF"].channel.image$attrs["rdf:resource"];
+        // 				const verification = {
+        // 					displayName: displayName,
+        // 					icon: icon,
+        // 					baseUrl: baseUrl
+        // 				};
+        // 				processVerification(verification);
+        // 			}
+        let feedUrl = baseUrl.split("/").splice(0, 3).join("/");
+        lookupIcon(feedUrl).then((icon) => {
+          const verification = {
+            displayName: displayName,
+            icon: icon,
+            baseUrl: baseUrl,
+          };
+          processVerification(verification);
+        });
+      } else {
+        // Unknown
+        processError(Error("Unknown feed format"));
+      }
+    })
+    .catch((requestError) => {
+      processError(requestError);
+    });
+}
+
+function load() {
+  const reqUrl = buildUrl();
+
+  let extraHeaders = [];
+  let lastModified = getItem("lastModified");
+  if (lastModified != null) {
+    console.log(`lastModified = ${lastModified}`);
+    extraHeaders["if-modified-since"] = lastModified;
+  }
+  let eTag = getItem("eTag");
+  if (eTag != null) {
+    console.log(`eTag = ${eTag}`);
+    extraHeaders["if-none-match"] = eTag;
+  }
+  extraHeaders["accept-encoding"] = "gzip,deflate";
+
+  sendRequest(reqUrl, "GET", null, extraHeaders, true)
+    .then((text) => {
+      let jsonObject = xmlParse(text);
+
+      if (jsonObject.feed != null) {
+        // Atom 1.0
+        const feedAttributes = jsonObject.feed.link$attrs;
+        let feedUrl = null;
+        if (feedAttributes instanceof Array) {
+          for (const feedAttribute of feedAttributes) {
+            if (feedAttribute.rel == "alternate") {
+              feedUrl = feedAttribute.href;
+              break;
+            }
+          }
+        } else if (feedAttributes.rel == "alternate") {
+          feedUrl = feedAttributes.href;
+        } else if (
+          jsonObject.feed.id.startsWith("http://") ||
+          jsonObject.feed.id.startsWith("https://")
+        ) {
+          feedUrl = jsonObject.feed.id;
+        }
+        const feedName = jsonObject.feed.title;
+
+        let entries = [];
+        if (jsonObject.feed.entry != null) {
+          const entry = jsonObject.feed.entry;
+          if (entry instanceof Array) {
+            entries = entry;
+          } else {
+            entries = [entry];
+          }
+        }
+        var results = [];
+        for (const entry of entries) {
+          const entryAttributes = entry.link$attrs;
+          let entryUrl = null;
+          if (entryAttributes instanceof Array) {
+            for (const entryAttribute of entryAttributes) {
+              if (entryAttribute.rel == "alternate") {
+                entryUrl = entryAttribute.href;
+                break;
+              }
+            }
+            // Posts need to have a link and if we didn't find one
+            // with rel == "alternate" then we'll use the first link.
+            if (!entryUrl && entryAttributes.length > 0) {
+              entryUrl = entryAttributes[0].href;
+            }
+          } else {
+            if (
+              entryAttributes.rel == "alternate" ||
+              entryAttributes.rel == null
+            ) {
+              entryUrl = entryAttributes.href;
+            }
+          }
+
+          const url = entryUrl;
+          let date = null;
+          if (entry.published) {
+            date = new Date(entry.published);
+          } else if (entry.updated) {
+            date = new Date(entry.updated);
+          }
+          const title = entry.title?.trim();
+
+          let content = "";
+          if (
+            entry.content$attrs != null &&
+            entry.content$attrs["type"] == "xhtml"
+          ) {
+            content = entry.content$xhtml;
+          } else {
+            content = entry.content ?? entry.summary;
+          }
+
+          var identity = null;
+          if (entry.author != null) {
+            let authorName = entry.author.name;
+            if (authorName != null) {
+              if (authorName instanceof Array) {
+                authorName = authorName.join(", ");
+              } else {
+                authorName = authorName.trim();
+              }
+              identity = Identity.createWithName(authorName);
+              if (entry.author.uri != null) {
+                identity.uri = entry.author.uri;
+              }
+            }
+          }
+
+          const resultItem = Item.createWithUriDate(url, date);
+          if (title != null) {
+            resultItem.title = title;
+          }
+          if (content != null) {
+            resultItem.body = content;
+          }
+          if (identity != null) {
+            resultItem.author = identity;
+          }
+          if (entryAttributes instanceof Array) {
+            const attachments = entryAttributes
+              .filter((e) => {
+                if (e.type) {
+                  // Check for a MIME type that suggests this is an image, e.g. image/jpeg.
+                  return e.type.startsWith("image/");
+                } else {
+                  return false;
+                }
+              })
+              .map((link) => {
+                const attachment = MediaAttachment.createWithUrl(link.href);
+                attachment.text = link.title || link.text;
+                attachment.mimeType = "image";
+                return attachment;
+              });
+            if (attachments.length > 0) {
+              resultItem.attachments = attachments;
+            }
+          } else {
+            // extract any media from RSS: https://www.rssboard.org/media-rss
+            if (entry["media:group"] != null) {
+              const mediaGroup = entry["media:group"];
+
+              const mediaAttributes = mediaGroup["media:thumbnail$attrs"];
+              let attachment = attachmentForAttributes(mediaAttributes);
+              if (attachment != null) {
+                resultItem.attachments = [attachment];
+              }
+            } else if (entry["media:thumbnail$attrs"] != null) {
+              const mediaAttributes = entry["media:thumbnail$attrs"];
+              let attachment = attachmentForAttributes(mediaAttributes);
+              if (attachment != null) {
+                resultItem.attachments = [attachment];
+              }
+            } else if (entry["media:content$attrs"] != null) {
+              const mediaAttributes = entry["media:content$attrs"];
+              let attachment = attachmentForAttributes(mediaAttributes);
+              if (attachment != null) {
+                resultItem.attachments = [attachment];
+              }
+            }
+          }
+
+          results.push(resultItem);
+        }
+
+        processResults(results);
+      } else if (jsonObject.rss != null && jsonObject.rss.channel != null) {
+        // RSS 2.0
+        const feedUrl = jsonObject.rss.channel.link;
+        const feedName = jsonObject.rss.channel.title;
+
+        let items = [];
+        if (jsonObject.rss.channel.item != null) {
+          const item = jsonObject.rss.channel.item;
+          if (item instanceof Array) {
+            items = item;
+          } else {
+            items = [item];
+          }
+        }
+
+        let results = [];
+        for (const item of items) {
+          const itemDate = item["pubDate"] ?? item["dc:date"];
+          if (item.link == null || itemDate == null) {
+            continue;
+          }
+          const url = item.link;
+          const date = new Date(itemDate);
+          let title = item.title?.trim();
+          let content = item["content:encoded"] ?? item.description;
+
+          let identity = null;
+          let authorName = item["dc:creator"];
+          if (authorName != null) {
+            if (authorName instanceof Array) {
+              authorName = authorName.join(", ");
+            } else {
+              authorName = authorName.trim();
+            }
+            identity = Identity.createWithName(authorName);
+            identity.uri = feedUrl;
+          }
+
+          const resultItem = Item.createWithUriDate(url, date);
+          if (title != null) {
+            resultItem.title = title;
+          }
+          if (content != null) {
+            resultItem.body = content;
+          }
+          if (identity != null) {
+            resultItem.author = identity;
+          }
+
+          let attachments = [];
+
+          // extract any media from RSS: https://www.rssboard.org/media-rss
+          if (item["media:group"] != null) {
+            const mediaGroup = item["media:group"];
+
+            const mediaAttributes = mediaGroup["media:thumbnail$attrs"];
+            let attachment = attachmentForAttributes(mediaAttributes);
+            if (attachment != null) {
+              attachments.push(attachment);
+            }
+          } else if (item["media:thumbnail$attrs"] != null) {
+            const mediaAttributes = item["media:thumbnail$attrs"];
+            let attachment = attachmentForAttributes(mediaAttributes);
+            if (attachment != null) {
+              attachments.push(attachment);
+            }
+          } else if (item["media:content$attrs"] != null) {
+            const mediaAttributes = item["media:content$attrs"];
+            let attachment = attachmentForAttributes(mediaAttributes);
+            if (attachment != null) {
+              attachments.push(attachment);
+            }
+          } else if (item["enclosure$attrs"] != null) {
+            let enclosure = item["enclosure$attrs"];
+            if (enclosure.url != null) {
+              let attachment = MediaAttachment.createWithUrl(enclosure.url);
+              attachments.push(attachment);
+            }
+          }
+
+          // add link attachment for link that isn't on this site (e.g. a link blog)
+          // but only if there isn't media already attached
+          if (attachments.length == 0) {
+            let linkPrefix = url.split("/").splice(0, 3).join("/");
+            let feedPrefix = feedUrl.split("/").splice(0, 3).join("/");
+            if (linkPrefix != feedPrefix) {
+              let attachment = LinkAttachment.createWithUrl(item["link"]);
+              attachments.push(attachment);
+            }
+          }
+
+          if (attachments.length > 0) {
+            resultItem.attachments = attachments;
+          }
+
+          results.push(resultItem);
+        }
+
+        processResults(results);
+      } else if (jsonObject["rdf:RDF"] != null) {
+        // RSS 1.0
+        const feedUrl = jsonObject["rdf:RDF"].channel.link;
+        const feedName = jsonObject["rdf:RDF"].channel.title;
+
+        const item = jsonObject["rdf:RDF"].item;
+        let items = null;
+        if (item instanceof Array) {
+          items = item;
+        } else {
+          items = [item];
+        }
+        var results = [];
+        for (const item of items) {
+          if (item["dc:date"] == null) {
+            continue;
+          }
+          const url = item.link;
+          const date = new Date(item["dc:date"]);
+          let title = item.title?.trim();
+          let content = item.description;
+
+          let identity = null;
+          let authorName = item["dc:creator"];
+          if (authorName != null) {
+            if (authorName instanceof Array) {
+              authorName = authorName.join(", ");
+            } else {
+              authorName = authorName.trim();
+            }
+            identity = Identity.createWithName(authorName);
+            identity.uri = feedUrl;
+          }
+
+          const resultItem = Item.createWithUriDate(url, date);
+          if (title != null) {
+            resultItem.title = title;
+          }
+          if (content != null) {
+            resultItem.body = content;
+          }
+          if (identity != null) {
+            resultItem.author = identity;
+          }
+
+          results.push(resultItem);
+        }
+
+        processResults(results);
+      } else {
+        // Unknown
+        processResults([]);
+      }
+    })
+    .catch((requestError) => {
+      console.log(requestError);
+      processError(requestError);
+    });
+}
+
+function attachmentForAttributes(mediaAttributes) {
+  let attachment = null;
+  if (mediaAttributes != null && mediaAttributes.url != null) {
+    attachment = MediaAttachment.createWithUrl(mediaAttributes.url);
+    if (mediaAttributes.width != null && mediaAttributes.height != null) {
+      let width = mediaAttributes.width;
+      let height = mediaAttributes.height;
+      attachment.aspectSize = { width: width, height: height };
+    }
+    attachment.mimeType = "image";
+  }
+  return attachment;
+}
